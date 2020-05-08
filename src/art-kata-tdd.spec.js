a1988f3bce920de1d4a31a269607080abe41e72d
epelde <aepelde@gmail.com>
2020-05-08 22:11:18 +0200

    artKataTdd returns a function

--- src/art-kata-tdd.spec.js
+++ src/art-kata-tdd.spec.js
@@ -4,4 +4,9 @@ describe('Art, Kata and Tdd', () => {
   it('is a function', () => {
     expect(typeof artKataTdd).toEqual('function');
   });
+
+  it('is a function that returns another function', () => {
+    const result = artKataTdd();
+    expect(typeof result).toEqual('function');
+  });
 });
